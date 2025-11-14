0b0059b094c8ee53251df5855ea92dee5101d775
JoaoRodrigues <joao1803208@gmail.com>
2025-11-14 08:09:49 -0300

    fodase

--- index.js
+++ index.js
@@ -1,107 +1,21 @@
-// index.js
-//require('dotenv').config();  // Para carregar as variáveis do .env
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const { createClient } = require('@supabase/supabase-js');
-const req = require('express/lib/request');
-dotenv.config();  // Carrega as variáveis do .env
-
-
+import express from "express";
+import fetch from "node-fetch";
 
 const app = express();
-const port = process.env.PORT || 3000;
-app.use(cors());
+app.use(express.json());
 
-// Configurando o cliente do Supabase
-const supabase = createClient(
-  process.env.SUPABASE_URL, 
-  process.env.SUPABASE_KEY
-);
+app.post("/sensor", async (req, res) => {
+  const resultado = await fetch("https://blcgszqtzogwdwbhgbwq.supabase.co/rest/v1/dados_sensor", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "apikey": process.env.SUPABASE_SERVICE_ROLE,
+      "Authorization": `Bearer ${process.env.SUPABASE_SERVICE_ROLE}`
+    },
+    body: JSON.stringify(req.body)
+  });
 
-// Endpoint de teste
-app.get('/', (req, res) => {
-  res.send('API está funcionando!');
+  res.sendStatus(resultado.status);
 });
 
-// Endpoint para pegar dados do Supabase
-app.get('/teste', async (req, res) => {
-  const { data, error } = await supabase
-    .from('teste') // Substitua pelo nome da sua tabela
-    .select('*');
-
-  if (error) {
-    return res.status(500).json({ error: error.message });
-  }
-  res.status(200).json(data);
-});
-
-//endpoint para verificar o usuario
-app.get('/usuario', async (req, res) => {
-  try {
-    const { data: usuario, error } = await supabase
-      .from('usuario')  // Nome da tabela correta
-      .select('*')
-      .eq('nome', {nome});  // Filtra pelo ID do usuário
-      
-    if (error) {
-      return res.status(500).json({ error: error.message });
-    }
-    res.status(200).json(usuario);  // Retorna os dados do usuário
-  } catch (err) {
-    res.status(500).json({ error: 'Erro interno no servidor.' });
-  }
-});
-
-//endpoint para o mapa
-app.get('/localizacao', async (req, res) => {
-  try {
-    const { data: localizacao, error } = await supabase
-      .from('coleta_de_dados')  // Nome da tabela correta
-      .select('latitude, longitude');
-
-    if (error) {
-      return res.status(500).json({ error: error.message });
-    }
-
-    res.status(200).json(localizacao);  // Retorna a localização
-  } catch (err) {
-    res.status(500).json({ error: 'Erro interno no servidor.' });
-  }
-});
-
-app.get('/batimentos', async (req, res) => {
-  try {
-    const { data: batimentos, error } = await supabase
-      .from('coleta_de_dados')  // Nome da tabela correta
-      .select('frequencia_cardiaca');
-
-    if (error) {
-      return res.status(500).json({ error: error.message });
-    }
-    res.status(200).json(batimentos);  // Retorna os batimentos
-  } catch (err) {
-    res.status(500).json({ error: 'Erro interno no servidor.' });
-  }
-});
-
-app.post('/cadastroPet', async (req, res) => {
-  try {
-    const { nome, raca, idade, peso, sexo, porte, vacinado, castrado, observacoes } = req.body;
-    const { data, error } = await supabase
-      .from('pets')  // Nome da tabela correta
-      .insert([{ nome, raca, idade, peso, sexo, porte, vacinado, castrado, observacoes }]);
-    if (error) {
-      return res.status(500).json({ error: error.message });
-    }
-    res.status(201).json(data);  // Retorna os dados do pet cadastrado
-  } catch (err) {
-    res.status(500).json({ error: 'Erro interno no servidor.' });
-  }
-});
-      
-
-// Inicia o servidor
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
-});
+app.listen(3000);
